1ae2ca476fd1e469a544fd62e92f562a0be8b5d2
aaron <vargheeseaaronv@gmail.com>
2024-04-16 18:05:13 +0530

    ui made better

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,3 @@
-import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -8,20 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'WeatherApp';
-  // constructor(private http:HttpClient){}
-  // city:string='';
-  // weatherData:any;
-  // getWeather(city?:string){
-  //   this.http.get(`https://weatherapi-com.p.rapidapi.com/current.json?q=${city}`,{
-  //     headers:{
-  //       'X-RapidAPI-Key': '9037939a91msh6613bfac2da4929p15ee2cjsn0cee449337cd',
-  //       'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-  //     }
-  //   }).subscribe((data)=>{
-  //       this.weatherData=data
-  //     })
-
-  //     console.warn(this.weatherData);
-      
-  // }
 }
--- src/app/components/homepage/homepage.component.ts
+++ src/app/components/homepage/homepage.component.ts
@@ -7,40 +7,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './homepage.component.css'
 })
 export class HomepageComponent implements OnInit{
-  // locationsecondMethod: any
-  // locationlatitude: any
-  // locationlongitude: any
+  locationsecondMethod: any
+  locationlatitude: any
+  locationlongitude: any
 
-  // weatherData: any;
+  weatherData: any;
 
-  // coords: any[] = [];
+  coords: any[] = [];
 
-  // constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
   ngOnInit(): void {
-  //   navigator.geolocation.getCurrentPosition((position) => {
-  //     console.log(position.coords)
-  //     this.locationsecondMethod = position.coords
-  //     this.locationlatitude = this.locationsecondMethod.latitude
-  //     this.locationlongitude = this.locationsecondMethod.longitude
-  //     console.log(this.locationlatitude);
-  //     console.log(this.locationlongitude);
+    navigator.geolocation.getCurrentPosition((position) => {
+      console.log(position.coords)
+      this.locationsecondMethod = position.coords
+      this.locationlatitude = this.locationsecondMethod.latitude
+      this.locationlongitude = this.locationsecondMethod.longitude
+      console.log(this.locationlatitude);
+      console.log(this.locationlongitude);
 
 
 
-  //     this.coords = [this.locationlatitude, this.locationlongitude]
-  //     console.warn(this.coords);
+      this.coords = [this.locationlatitude, this.locationlongitude]
+      console.warn(this.coords);
 
-  //     this.http.get(`https://weatherapi-com.p.rapidapi.com/current.json?q=${this.coords}`, {
-  //       headers: {
-  //         'X-RapidAPI-Key': '9037939a91msh6613bfac2da4929p15ee2cjsn0cee449337cd',
-  //         'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-  //       }
-  //     }).subscribe((data) => {
-  //       console.log(data);
+      this.http.get(`https://weatherapi-com.p.rapidapi.com/current.json?q=${this.coords}`, {
+        headers: {
+          'X-RapidAPI-Key': '9037939a91msh6613bfac2da4929p15ee2cjsn0cee449337cd',
+          'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+        }
+      }).subscribe((data) => {
+        console.log(data);
 
-  //       this.weatherData = data;
-  //     })
-  //   })
+        this.weatherData = data;
+      })
+    })
 
 
   }
